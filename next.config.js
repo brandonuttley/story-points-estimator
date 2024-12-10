f7113501b40a77fe3bab2ad5bb92ba19e5d9768c
Brandon Uttley <brandonuttley@users.noreply.github.com>
2024-12-10 10:30:06 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/story-points-estimator',
-    distDir: 'out',
-  }
-  
-  module.exports = nextConfig
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/story-points-estimator',
+  distDir: 'out',
+}
+
+module.exports = nextConfig
